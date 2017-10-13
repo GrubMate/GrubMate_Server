6f42d9967382340d55ad1da38e281ed55cf77793
zhaoqi <zhaoqizh@usc.edu>
2017-10-12 20:05:34 -0700

    Obj class created

---
+++ src/main/java/dataClass/Post.java
@@ -0,0 +1,16 @@
+package dataClass;
+
+public class Post {
+    public Integer postID;
+    public Integer posterID;
+    public String[] postPhotos;
+    public String[] tags;
+    public String[] timePeriod;
+    public String description;
+    public Double[] address;
+    public Integer totalQuantity;
+    public Integer leftQuantity;
+    public Integer requestsIDs;
+    public Boolean isActive;
+    public Boolean[] allergyInfo;
+}
