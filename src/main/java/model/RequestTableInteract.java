86b5cff90c5b43ef67d747967753ef81b1480e94
Jingxing Yang <jingxiny@usc.edu>
2017-10-13 18:39:12 -0700

    update

--- src/main/java/model/RequestTableInteract.java
+++ src/main/java/model/RequestTableInteract.java
@@ -6,13 +6,21 @@ import dataClass.Request;
 public class RequestTableInteract {
     public BasicDBObject addRequest(Request newRequest)
     {
+        BasicDBObject request = toRequestObj(newRequest);
+        SharedObject.mi.requestTable.insert(request);
+        return request;
+    }
+
+    
+
+    public BasicDBObject toRequestObj(Request newRequest)
+    {
         BasicDBObject request = new BasicDBObject();
         request.put(Request.REQUEST_ID, newRequest.requestID );
         request.put(Request.REQUESTER_ID,newRequest.requesterID);
         request.put(Request.TARGET_POST_ID,newRequest.targetPostID);
         request.put(Request.STATUS,newRequest.status);
         request.put(Request.ADDRESS,newRequest.address);
-        SharedObject.mi.requestTable.insert(request);
         return request;
     }
 }
