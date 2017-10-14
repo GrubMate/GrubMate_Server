09424e9aa2dda81c76c6d514cc3cb8a7af1f110a
Jingxing Yang <jingxiny@usc.edu>
2017-10-13 17:40:44 -0700

    add reqeustTableInteract

---
+++ src/main/java/model/RequestTableInteract.java
@@ -0,0 +1,18 @@
+package model;
+
+import com.mongodb.BasicDBObject;
+import dataClass.Request;
+
+public class RequestTableInteract {
+    public BasicDBObject addRequest(Request newRequest)
+    {
+        BasicDBObject request = new BasicDBObject();
+        request.put(Request.REQUEST_ID, newRequest.requestID );
+        request.put(Request.REQUESTER_ID,newRequest.requesterID);
+        request.put(Request.TARGET_POST_ID,newRequest.targetPostID);
+        request.put(Request.STATUS,newRequest.status);
+
+        SharedObject.mi.requestTable.insert(request);
+        return request;
+    }
+}
