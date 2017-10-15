f3af30f0b8588c7982d28fd19f6b20ceb0290730
Jingxing Yang <jingxiny@usc.edu>
2017-10-15 16:15:31 -0700

    added searchReqeust

---
+++ src/main/java/dataClass/SearchRequest.java
@@ -0,0 +1,10 @@
+package dataClass;
+
+public class SearchRequest {
+    public int userID;
+    public String title;
+    public String category;
+    public String[] tags;
+    public int[] time;
+    public String description;
+}
