77cb770be6e674cb5a599dfd2259abda64f13f3d
SeasonJi <jiej@usc.edu>
2017-10-14 22:06:14 -0700

    sa

--- src/main/java/model/UserTableInteract.java
+++ src/main/java/model/UserTableInteract.java
@@ -1,7 +1,10 @@
 package model;
 
 
+import com.google.gson.Gson;
 import com.mongodb.*;
+import com.mongodb.util.JSON;
+import dataClass.Post;
 import dataClass.User;
 
 import java.util.ArrayList;
@@ -9,9 +12,59 @@ import java.util.List;
 
 public class UserTableInteract {
 
-    static public BasicDBObject addUser(User usr)
+    static public BasicDBObject addUser(User u)
     {
-        return null;
+        String usr = new Gson().toJson(u);
+
+        BasicDBObject obj = (BasicDBObject) JSON.parse(usr);
+
+        System.out.println("Entering addUser");
+
+        //int newID = SharedObject.mi.incrementTargetID("postID");
+
+
+        BasicDBObject checkFBid = new BasicDBObject(User.FACEBOOK_ID, obj.get(User.FACEBOOK_ID));
+
+        SharedObject.mi.userCursor = SharedObject.mi.userTable.find(checkFBid);
+
+        int newID;
+        if(SharedObject.mi.userCursor.hasNext())
+        {
+            updateUser(usr);
+            System.out.println("a new user with ID: " + obj.get(User.USER_ID) + " is now being updated");
+
+        }
+        else
+        {
+            newID = IDCounter.incrementTargetID(IDCounter.USER);
+            obj.append(User.USER_ID, newID);
+            SharedObject.mi.userTable.insert(obj);
+
+            System.out.println("a new user with ID: " + obj.get(User.USER_ID));
+
+        }
+
+
+
+        return obj;
+    }
+
+
+    static public void updateUser(String user)
+    {
+        BasicDBObject obj = (BasicDBObject) JSON.parse(user);
+
+        BasicDBObject query = new BasicDBObject();
+        query.put(User.USER_ID, obj.get(User.USER_ID));
+
+
+
+        BasicDBObject updateObj = new BasicDBObject();
+        updateObj.put("$set", obj);
+
+
+        SharedObject.mi.userTable.update(query, updateObj);
+
     }
 
     public BasicDBObject getUser(User user)
@@ -49,15 +102,17 @@ public class UserTableInteract {
 
         //uti.clearUserTable();
 
-//        User any = new User();
-//
-//        any.userName = "dads";
-//        any.facebookID = "ddafqwd";
-//        any.profilePhoto = "https:wadwad.dadda.com";
-//        any.bio = "This user is lazy. No bio!";
-//
-//        uti.addUser(any);
+        User any = new User();
+        any.userID = 9;
+        any.userName = "qwqewqew";
+        any.facebookID = "iii";
+        any.profilePhoto = "https:dadada.dadda.com";
+        any.bio = "This user i!";
 
+        //uti.addUser(any);
+
+        String s = new Gson().toJson(any);
+        uti.updateUser(s);
 //        BasicDBObject bo = new BasicDBObject();
 //        bo = mi.getUser(123321);
 //        System.out.println(bo);
