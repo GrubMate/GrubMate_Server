0eca606f11eb00d796bc31aacf07aa2f1555b367
zhaoqi <zhaoqizh@usc.edu>
2017-10-15 18:17:14 -0700

    S

--- src/main/java/controller/FriendController.java
+++ src/main/java/controller/FriendController.java
@@ -1,9 +1,14 @@
 package controller;
 
 
+import dataClass.User;
+import javafx.util.Pair;
 import model.PostTableInteract;
+import model.UserTableInteract;
 import org.springframework.web.bind.annotation.*;
 import dataClass.Post;
+
+import java.util.ArrayList;
 
 
 @RestController
@@ -12,9 +17,20 @@ public class FriendController {
 
 
     @RequestMapping(value="/{id}",method= RequestMethod.GET)
-    public Integer[] get(@PathVariable("id") Integer uid){
-        Integer[] friends = {};
-        return friends;
+    public ArrayList<Pair<Integer,String>> get(@PathVariable("id") Integer uid){
+        User user = UserTableInteract.getUser(uid);
+        ArrayList<Integer> allFriends = user.allFriends;
+        ArrayList<Pair<Integer,String>> feed = new ArrayList<Pair<Integer,String>>();
+        if (allFriends!=null) {
+            for (int i=0;i<allFriends.size();i++) {
+                User friend = UserTableInteract.getUser(allFriends.get(i));
+                if (friend!=null) {
+                    feed.add(new Pair<Integer, String>(friend.userID,friend.userName));
+                }
+
+            }
+        }
+        return feed;
     }
 
 
